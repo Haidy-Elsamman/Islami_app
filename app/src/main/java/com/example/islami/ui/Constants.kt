1258a37d0776c339773f9924bbb0fee12be644da
Haidy <haidyahmedelsamman@gmail.com>
2023-03-05 02:57:06 +0200

    + Sebha Fragment

--- app/src/main/java/com/example/islami/ui/Constants.kt
+++ app/src/main/java/com/example/islami/ui/Constants.kt
@@ -4,5 +4,9 @@ object Constants {
 
     val EXTRA_SURA_NAME = "name"
     val EXTRA_SURA_POSITION = "position"
+    const val SUBHAN_ALLAH = "سبحان الله"
+    const val ELHAMDU_ALLAH = "الحمدلله"
+    const val ALLAH_AKBAR = "الله اكبر"
+    const val KHATMEA = "لا اله الا الله وحده لا شريك له"
 
 }
--- app/src/main/java/com/example/islami/ui/home/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/example/islami/ui/home/tasbeh/TasbehFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.islami.databinding.FragmentTasbehBinding
+import com.example.islami.ui.Constants
 
 class TasbehFragment : Fragment() {
     lateinit var binding: FragmentTasbehBinding
+    var counter: Int = 0
+    lateinit var counterTextView: String
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,5 +20,40 @@ class TasbehFragment : Fragment() {
     ): View? {
         binding = FragmentTasbehBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.countTasbeeh.text = "$counter"
+        binding.bodyOfSebha.setOnClickListener {
+            onSebhaClicked()
+        }
+    }
+
+    private fun onSebhaClicked() {
+        binding.bodyOfSebha.rotation += 5
+        counter += 1
+        binding.countTasbeeh.text = "$counter"
+
+        if (binding.phases.text == Constants.KHATMEA) {
+            binding.phases.text = Constants.SUBHAN_ALLAH
+            counter = 0
+            binding.countTasbeeh.text = "$counter"
+        }
+        if (counter == 33) {
+            if (binding.phases.text == Constants.SUBHAN_ALLAH) {
+                binding.phases.text = Constants.ELHAMDU_ALLAH
+                counter = 0
+                binding.countTasbeeh.text = "$counter"
+            } else if (binding.phases.text == Constants.ELHAMDU_ALLAH) {
+                counter = 0
+                binding.phases.text = Constants.ALLAH_AKBAR
+                binding.countTasbeeh.text = "$counter"
+            } else if (binding.phases.text == Constants.ALLAH_AKBAR) {
+                counter = 0
+                binding.phases.text = Constants.KHATMEA
+                binding.countTasbeeh.text = "$counter"
+            }
+        }
     }
 }
